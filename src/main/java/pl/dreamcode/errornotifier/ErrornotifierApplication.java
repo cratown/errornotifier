bc5cc397e6d019f918afa1e42e3ef7a8cdfcb953
Łukasz Kaczmarek <luke@dreamcode.pl>
2024-08-14 16:01:50 +0200

    Some cleanup

--- src/main/java/pl/dreamcode/errornotifier/ErrornotifierApplication.java
+++ src/main/java/pl/dreamcode/errornotifier/ErrornotifierApplication.java
@@ -2,7 +2,6 @@ package pl.dreamcode.errornotifier;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,5 +12,4 @@ public class ErrornotifierApplication implements WebMvcConfigurer  {
 	public static void main(String[] args) {
 		SpringApplication.run(ErrornotifierApplication.class, args);
 	}
-
 }
